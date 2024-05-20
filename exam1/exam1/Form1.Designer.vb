26dccd36a128e914d5ed4ace40e2bfeaa814cb08
CafePol <119393703+CfPol@users.noreply.github.com>
2024-05-20 16:30:52 +0800

    Add files via upload

---
+++ exam1/exam1/Form1.Designer.vb
@@ -0,0 +1,154 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.course_cbo = New System.Windows.Forms.ComboBox()
+        Me.sub_cbo = New System.Windows.Forms.ComboBox()
+        Me.name_cbo = New System.Windows.Forms.ComboBox()
+        Me.save_btn = New System.Windows.Forms.Button()
+        Me.add_btn = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(83, 94)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(60, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Course"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(83, 148)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(107, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "student name"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(83, 202)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(120, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "subject to enroll"
+        '
+        'course_cbo
+        '
+        Me.course_cbo.FormattingEnabled = True
+        Me.course_cbo.Location = New System.Drawing.Point(316, 93)
+        Me.course_cbo.Name = "course_cbo"
+        Me.course_cbo.Size = New System.Drawing.Size(304, 21)
+        Me.course_cbo.TabIndex = 3
+        '
+        'sub_cbo
+        '
+        Me.sub_cbo.FormattingEnabled = True
+        Me.sub_cbo.Location = New System.Drawing.Point(316, 201)
+        Me.sub_cbo.Name = "sub_cbo"
+        Me.sub_cbo.Size = New System.Drawing.Size(304, 21)
+        Me.sub_cbo.TabIndex = 4
+        '
+        'name_cbo
+        '
+        Me.name_cbo.FormattingEnabled = True
+        Me.name_cbo.Location = New System.Drawing.Point(316, 147)
+        Me.name_cbo.Name = "name_cbo"
+        Me.name_cbo.Size = New System.Drawing.Size(304, 21)
+        Me.name_cbo.TabIndex = 5
+        '
+        'save_btn
+        '
+        Me.save_btn.Location = New System.Drawing.Point(428, 531)
+        Me.save_btn.Name = "save_btn"
+        Me.save_btn.Size = New System.Drawing.Size(75, 23)
+        Me.save_btn.TabIndex = 6
+        Me.save_btn.Text = "save"
+        Me.save_btn.UseVisualStyleBackColor = True
+        '
+        'add_btn
+        '
+        Me.add_btn.Location = New System.Drawing.Point(704, 202)
+        Me.add_btn.Name = "add_btn"
+        Me.add_btn.Size = New System.Drawing.Size(75, 23)
+        Me.add_btn.TabIndex = 7
+        Me.add_btn.Text = "add subject"
+        Me.add_btn.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.AllowUserToResizeColumns = False
+        Me.DataGridView1.AllowUserToResizeRows = False
+        Me.DataGridView1.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.Disable
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(87, 267)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersVisible = False
+        Me.DataGridView1.Size = New System.Drawing.Size(533, 231)
+        Me.DataGridView1.TabIndex = 8
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(959, 600)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.add_btn)
+        Me.Controls.Add(Me.save_btn)
+        Me.Controls.Add(Me.name_cbo)
+        Me.Controls.Add(Me.sub_cbo)
+        Me.Controls.Add(Me.course_cbo)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents course_cbo As System.Windows.Forms.ComboBox
+    Friend WithEvents sub_cbo As System.Windows.Forms.ComboBox
+    Friend WithEvents name_cbo As System.Windows.Forms.ComboBox
+    Friend WithEvents save_btn As System.Windows.Forms.Button
+    Friend WithEvents add_btn As System.Windows.Forms.Button
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+
+End Class
---
+++ exam1/exam1/Form1.vb
@@ -0,0 +1,267 @@
+﻿Imports MySql.Data.MySqlClient
+
+Module Module1
+    Public cn As New MySqlConnection
+    Public cm As New MySqlCommand
+    Public cm1 As New MySqlCommand
+    Public dr As MySqlDataReader
+    Public dt As New DataTable
+    Public dt1 As New DataTable
+    Public da As MySqlDataAdapter
+    Public da1 As MySqlDataAdapter
+    Public ds As DataSet
+    Public Function ConnectToDB() As MySqlConnection
+        Dim connection As New MySqlConnection
+        Try
+            connection.ConnectionString = "server=localhost; user id=root; password=; database=exam"
+            connection.Open()
+            Return connection
+        Catch ex As Exception
+            Return Nothing
+        End Try
+    End Function
+
+End Module
+
+Module DBConnection
+
+    Public Function strconnection() As MySqlConnection
+        Return New MySqlConnection("server=localhost; user id=root; password=; database=exam")
+    End Function
+
+    Public strcon As MySqlConnection = strconnection()
+
+    Public result As String
+    Public cmd As New MySqlCommand
+    Public da As MySqlDataAdapter
+    Public dt As New DataTable
+
+    Public UserType As String
+
+    Public Sub create(ByVal sql As String)
+        Try
+            strcon.Open()
+            With cmd
+                .Connection = strcon
+                .CommandText = sql
+                result = cmd.ExecuteNonQuery
+                If result = 0 Then
+                    MessageBox.Show("Failed to save data", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                Else
+                    MessageBox.Show("Data saved successfully", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                End If
+            End With
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            strcon.Close()
+        End Try
+    End Sub
+
+    Public Sub reload(ByVal sql As String, ByVal DTG As Object)
+        Try
+            If DTG Is Nothing Then
+                MessageBox.Show("Error: The DataGridView object is null.")
+                Return
+            End If
+
+            dt = New DataTable
+            strcon.Open()
+
+
+            If da Is Nothing Then
+                da = New MySqlDataAdapter
+            End If
+
+
+            
+
+            With cmd
+                .Connection = strcon
+                .CommandText = sql
+            End With
+
+            da.SelectCommand = cmd
+            da.Fill(dt)
+            DTG.DataSource = dt
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            strcon.Close()
+            If da IsNot Nothing Then
+                da.Dispose()
+            End If
+        End Try
+    End Sub
+
+
+    Public Sub updates(ByVal sql As String)
+        Try
+            strcon.Open()
+            With cmd
+                .Connection = strcon
+                .CommandText = sql
+                result = cmd.ExecuteNonQuery
+                If result = 0 Then
+                    MessageBox.Show("Data failed to update", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                Else
+                    MessageBox.Show("Data updated successfully", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                End If
+            End With
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            strcon.Close()
+        End Try
+    End Sub
+
+    Public Sub delete(ByVal sql As String)
+        Try
+            strcon.Open()
+            With cmd
+                .Connection = strcon
+                .CommandText = sql
+                result = cmd.ExecuteNonQuery
+                If result = 0 Then
+                    MessageBox.Show("Data failed to delete in database", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                Else
+                    MessageBox.Show("Data deleted successfully", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                End If
+            End With
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            strcon.Close()
+        End Try
+    End Sub
+
+End Module
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        LoadCourseData()
+        LoadSubjectData()
+        DataGridView1.Columns.Add("Column1", "Subject_id")
+        DataGridView1.Columns.Add("Column2", "Lecturer")
+
+    End Sub
+
+    Private Sub LoadCourseData()
+
+        Dim sql As String = "SELECT Course_name FROM courses"
+
+        reload(sql, course_cbo)
+
+
+        course_cbo.DisplayMember = "Course_name"
+    End Sub
+
+
+    Private Sub course_cbo_SelectedIndexChanged(sender As Object, e As EventArgs) Handles course_cbo.SelectedIndexChanged
+        If course_cbo.SelectedItem IsNot Nothing Then
+            Dim selectedCourseId As String = course_cbo.SelectedValue.ToString()
+            ' Call LoadStudentData with the selected course ID
+            LoadStudentData(selectedCourseId)
+        End If
+    End Sub
+
+
+    Private Sub LoadStudentData(courseId As String)
+
+        Dim sql As String = "SELECT Stud_name FROM students WHERE course = @courseId"
+        reload(sql, name_cbo)
+        name_cbo.DisplayMember = "Stud_name"
+    End Sub
+
+
+    Private Sub LoadSubjectData()
+        Dim sql As String = "SELECT subjects.subject_id, subjects.subject_name, lecturer.lecturer_name " &
+                            "FROM subjects " &
+                            "INNER JOIN lecturer ON subjects.lecturer = lecturer.lecturer_id"
+
+        reload(sql, sub_cbo)
+        sub_cbo.DisplayMember = "subject_name"
+        sub_cbo.ValueMember = "subject_id"
+    End Sub
+
+    Private Sub btn_add_Click(sender As Object, e As EventArgs) Handles add_btn.Click
+        If course_cbo.SelectedItem Is Nothing OrElse name_cbo.SelectedItem Is Nothing OrElse course_cbo.SelectedItem Is Nothing Then
+            MessageBox.Show("Please select a course, a student, and a subject to enroll.")
+            Return
+        End If
+
+        Dim selectedCourse As String = course_cbo.SelectedItem.ToString()
+        Dim selectedStudent As String = name_cbo.SelectedItem.ToString()
+
+        ' Cast the selected item to DataRowView to access its fields
+        Dim selectedSubjectRow As DataRowView = DirectCast(sub_cbo.SelectedItem, DataRowView)
+        Dim selectedSubjectId As String = selectedSubjectRow("subject_id").ToString()
+        Dim selectedSubjectName As String = selectedSubjectRow("subject_name").ToString()
+        Dim selectedLecturerName As String = selectedSubjectRow("lecturer_name").ToString()
+
+
+        DataGridView1.Rows.Add(selectedSubjectId, selectedLecturerName)
+    End Sub
+
+
+    Private Sub save(sender As Object, e As EventArgs) Handles save_btn.Click
+        If course_cbo.SelectedItem Is Nothing OrElse name_cbo.SelectedItem Is Nothing OrElse course_cbo.SelectedItem Is Nothing Then
+            MessageBox.Show("Please select a course, a student, and a subject to enroll.")
+            Return
+        End If
+
+        Dim selectedStudentName As String = name_cbo.Text
+
+        ' Fetch student_id corresponding to the selected student name
+        Dim selectedStudentId As String = GetStudentId(selectedStudentName)
+
+        ' Iterate over DataGridView rows and insert data into student_subject table
+        For Each row As DataGridViewRow In DataGridView1.Rows
+            Dim subjectId As String = row.Cells("Column1").Value
+
+            ' Insert data into student_subject table with student_id and subject_id
+            InsertStudentSubject(selectedStudentId, subjectId)
+            
+
+        Next
+        MessageBox.Show("Data inserted Successfully", "Welcome!", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        DataGridView1.Rows.Clear()
+    End Sub
+
+    Private Function GetStudentId(studentName As String) As String
+        Dim studentId As String = ""
+
+        ' Query to get student_id from students table based on student_name
+        Dim sql As String = "SELECT stud_id FROM students WHERE Stud_name = @studentName"
+        Using connection As New MySqlConnection("server=localhost; user id=root; password=; database=exam")
+            Using command As New MySqlCommand(sql, connection)
+                command.Parameters.AddWithValue("@studentName", studentName)
+                connection.Open()
+                Dim reader As MySqlDataReader = command.ExecuteReader()
+                If reader.Read() Then
+                    studentId = reader("Stud_id").ToString()
+                    Return studentId
+                End If
+            End Using
+        End Using
+
+        Return studentId
+    End Function
+
+    Private Sub InsertStudentSubject(studentId As String, subjectId As String)
+        ' Insert data into student_subject table with student_id and subject_id
+        Dim sql As String = "INSERT INTO student_subject (student_id, subject_id) VALUES (@studentId, @subjectId)"
+        Using connection As New MySqlConnection("server=localhost; user id=root; password=; database=exam")
+            Using command As New MySqlCommand(sql, connection)
+                command.Parameters.AddWithValue("@studentId", studentId)
+                command.Parameters.AddWithValue("@subjectId", subjectId)
+                connection.Open()
+                command.ExecuteNonQuery()
+            End Using
+        End Using
+    End Sub
+
+
+
+End Class
