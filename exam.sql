50c63f0ef82614e8b39003fedba830d20b9b1fd2
CafePol <119393703+CfPol@users.noreply.github.com>
2024-05-20 16:20:43 +0800

    Add files via upload

--- exam.sql
+++ exam.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 20, 2024 at 08:06 AM
+-- Generation Time: May 20, 2024 at 09:09 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -37,7 +37,11 @@ CREATE TABLE `courses` (
 --
 
 INSERT INTO `courses` (`Course_id`, `Course_name`) VALUES
-(1, 'Robotics');
+(1, 'Robotics'),
+(2, 'Information Technology'),
+(3, 'Accountancy'),
+(4, 'Information Technology'),
+(5, 'Accountancy');
 
 -- --------------------------------------------------------
 
@@ -56,7 +60,8 @@ CREATE TABLE `lecturer` (
 --
 
 INSERT INTO `lecturer` (`Lecturer_id`, `Lecturer_name`, `Department_id`) VALUES
-(1, 'Fox Burger', 1);
+(1, 'Fox Burger', 1),
+(2, 'Kian Valencia', 2);
 
 -- --------------------------------------------------------
 
@@ -77,7 +82,8 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`Stud_id`, `Stud_name`, `Stud_address`, `Birthday`, `Course`) VALUES
-(1, 'Paul Andrei', 'labo', '0000-00-00', 1);
+(1, 'Paul Andrei', 'labo', '0000-00-00', 1),
+(2, 'Gerry Flores', 'Panganiban', '2004-03-24', 5);
 
 -- --------------------------------------------------------
 
@@ -88,9 +94,29 @@ INSERT INTO `students` (`Stud_id`, `Stud_name`, `Stud_address`, `Birthday`, `Cou
 CREATE TABLE `student_subject` (
   `Stud_sub_id` int(11) NOT NULL,
   `student_id` int(11) NOT NULL,
-  `subject_id` int(11) NOT NULL,
+  `subject_id` varchar(250) NOT NULL,
   `Grade` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `student_subject`
+--
+
+INSERT INTO `student_subject` (`Stud_sub_id`, `student_id`, `subject_id`, `Grade`) VALUES
+(42, 1, '4', 0),
+(43, 1, '3', 0),
+(44, 1, '1', 0),
+(45, 1, '0', 0),
+(46, 1, '0', 0),
+(47, 1, 'IT-101', 0),
+(48, 1, 'IT-101', 0),
+(49, 2, '3', 0),
+(50, 2, 'IT-101', 0),
+(51, 2, 'IT-101', 0),
+(52, 2, '2', 0),
+(53, 2, '3', 0),
+(54, 1, 'IT-101', 0),
+(55, 1, '3', 0);
 
 -- --------------------------------------------------------
 
@@ -99,7 +125,7 @@ CREATE TABLE `student_subject` (
 --
 
 CREATE TABLE `subjects` (
-  `Subject_id` int(11) NOT NULL,
+  `Subject_id` varchar(250) NOT NULL,
   `Subject_name` varchar(250) NOT NULL,
   `Lecturer` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -109,7 +135,10 @@ CREATE TABLE `subjects` (
 --
 
 INSERT INTO `subjects` (`Subject_id`, `Subject_name`, `Lecturer`) VALUES
-(1, 'robotics', 1);
+('1', 'robotics', 1),
+('2', 'DBMS', 1),
+('3', 'Mobile App', 1),
+('IT-101', 'fundamentals of programming', 2);
 
 --
 -- Indexes for dumped tables
@@ -131,16 +160,13 @@ ALTER TABLE `lecturer`
 -- Indexes for table `students`
 --
 ALTER TABLE `students`
-  ADD PRIMARY KEY (`Stud_id`),
-  ADD KEY `fk_3` (`Course`);
+  ADD PRIMARY KEY (`Stud_id`);
 
 --
 -- Indexes for table `student_subject`
 --
 ALTER TABLE `student_subject`
-  ADD PRIMARY KEY (`Stud_sub_id`),
-  ADD KEY `fk_2` (`subject_id`),
-  ADD KEY `fk_4` (`student_id`);
+  ADD PRIMARY KEY (`Stud_sub_id`);
 
 --
 -- Indexes for table `subjects`
@@ -157,31 +183,25 @@ ALTER TABLE `subjects`
 -- AUTO_INCREMENT for table `courses`
 --
 ALTER TABLE `courses`
-  MODIFY `Course_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `Course_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `lecturer`
 --
 ALTER TABLE `lecturer`
-  MODIFY `Lecturer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `Lecturer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `Stud_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `Stud_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `student_subject`
 --
 ALTER TABLE `student_subject`
-  MODIFY `Stud_sub_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `subjects`
---
-ALTER TABLE `subjects`
-  MODIFY `Subject_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `Stud_sub_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
 
 --
 -- Constraints for dumped tables
@@ -192,13 +212,6 @@ ALTER TABLE `subjects`
 --
 ALTER TABLE `students`
   ADD CONSTRAINT `fk_3` FOREIGN KEY (`Course`) REFERENCES `courses` (`Course_id`);
-
---
--- Constraints for table `student_subject`
---
-ALTER TABLE `student_subject`
-  ADD CONSTRAINT `fk_2` FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`Subject_id`),
-  ADD CONSTRAINT `fk_4` FOREIGN KEY (`student_id`) REFERENCES `students` (`Stud_id`);
 
 --
 -- Constraints for table `subjects`
